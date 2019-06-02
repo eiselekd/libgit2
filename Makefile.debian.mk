47819c38600b337dcd4a999453723451cf518c94
Konrad Eisele <eiselekd@gmail.com>
2019-06-02 21:33:23 +0200

    all

--- Makefile.debian.mk
+++ Makefile.debian.mk
@@ -1,6 +1,6 @@
 
 all:
-	DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -us -uc -b
+	DEB_BUILD_OPTIONS='nocheck nostrip noopt debug' dpkg-buildpackage -us -uc -b
 
 prep:
 	sudo apt-get install cmake libkrb5-dev libssh2-1-dev libmbedtls-dev
